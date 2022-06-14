e70bc3101cdcdaf4de5757a0986099646a536469
ProsteMartin <74150428+ProsteMartin@users.noreply.github.com>
2022-06-14 18:11:44 +0200

    Update basicanims.lua

--- basicanims.lua
+++ basicanims.lua
@@ -1,42 +1,40 @@
+-- CROSSARMS
+
 local crossarms = false
+
 RegisterCommand('crossarms', function()
-    if not IsEntityPlayingAnim(PlayerPedId(), "anim@heists@narcotics@funding@gang_idle", "gang_chatting_idle01", 3) then
-        if not crossarms then
-            local dict = "anim@amb@nightclub@peds@"
-            RequestAnimDict(dict)
-            while not HasAnimDictLoaded(dict) do
-                Wait(500)
-            end
-            TaskPlayAnim(PlayerPedId(), dict, "rcmme_amanda1_stand_loop_cop", 2.5, 2.5, -1, 50, 0, false, false, false)
-            crossarms = true
-        else
-            crossarms = false
-            ClearPedTasks(PlayerPedId())
+	if not crossarms then
+        local dict = "anim@amb@nightclub@peds@"
+        RequestAnimDict(dict)
+        while not HasAnimDictLoaded(dict) do
+            Wait(500)
         end
+        TaskPlayAnim(PlayerPedId(), dict, "rcmme_amanda1_stand_loop_cop", 2.5, 2.5, -1, 50, 0, false, false, false)
+        crossarms = true
     else
-	Wait(500)
+        crossarms = false
+        ClearPedTasks(PlayerPedId())
     end
 end)
 
 RegisterKeyMapping('crossarms', 'Crossarms', 'keyboard', 'g')
 
+-- HANDSUP
+
 local handsup = false
+
 RegisterCommand('handsup', function()
-    if not IsEntityPlayingAnim(PlayerPedId(), "anim@heists@narcotics@funding@gang_idle", "gang_chatting_idle01", 3) then
-        if not handsup then
-            local dict = "random@mugging3"
-            RequestAnimDict(dict)
-            while not HasAnimDictLoaded(dict) do
-                Wait(500)
-            end
-            TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 2.5, 2.5, -1, 50, 0, false, false, false)
-            handsup = true
-        else
-            handsup = false
-            ClearPedTasks(PlayerPedId())
+    if not handsup then
+        local dict = "random@mugging3"
+        RequestAnimDict(dict)
+        while not HasAnimDictLoaded(dict) do
+            Wait(500)
         end
+        TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 2.5, 2.5, -1, 50, 0, false, false, false)
+        handsup = true
     else
-	Wait(500)
+        handsup = false
+        ClearPedTasks(PlayerPedId())
     end
 end)
 
