0bdc6ef70ec50c1612063f75d5c5b53b3e244991
Martin <74150428+ProsteMartin@users.noreply.github.com>
2022-10-22 21:50:20 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,17 @@
 Config = {}
 
+-- Hands Up
 Config.HandsupBind = "X"
+Config.HandsupName = "Hands Up" -- Name in Settings - Key Bindings - FiveM
+
+-- Crossarms
 Config.CrossarmsBind = "G"
+Config.CrossarmsName = "Crossarms" -- Name in Settings - Key Bindings - FiveM
+
+-- Crouch
 Config.CrouchBind = "C" -- If you want it on CTRL, use LCONTROL.
+Config.CrouchName = "Crouch" -- Name in Settings - Key Bindings - FiveM
+
+-- PointFinger
 Config.PointFingerBind = "B"
+Config.PointFingerName = "Point Finger" -- Name in Settings - Key Bindings - FiveM
