45447218c65c565bcdb77c49f0efede4e11fae1e
Martin <74150428+ProsteMartin@users.noreply.github.com>
2022-10-22 21:45:56 +0200

    Update cl_handsup.lua

---
+++ anims/cl_handsup.lua
@@ -0,0 +1,19 @@
+-- HANDSUP
+local handsup = false
+
+RegisterCommand('handsup', function()
+    if not handsup then
+        local dict = "random@mugging3"
+        RequestAnimDict(dict)
+        while not HasAnimDictLoaded(dict) do
+            Wait(500)
+        end
+        TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 2.5, 2.5, -1, 50, 0, false, false, false)
+        handsup = true
+    else
+        handsup = false
+        ClearPedTasks(PlayerPedId())
+    end
+end)
+
+RegisterKeyMapping('handsup', 'Handsup', 'keyboard', Config.HandsupBind)
