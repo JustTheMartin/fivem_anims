616937653dcc40c8bc5cc4b04e5b55877458d9bd
Martin <74150428+ProsteMartin@users.noreply.github.com>
2022-10-22 22:36:04 +0200

    Create cl_handsup.lua

--- anims/cl_handsup.lua
+++ anims/cl_handsup.lua
@@ -9,6 +9,7 @@ RegisterCommand('handsup', function()
             Wait(500)
         end
         TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 2.5, 2.5, -1, 50, 0, false, false, false)
+        RemoveAnimDict(dict)
         handsup = true
     else
         handsup = false
