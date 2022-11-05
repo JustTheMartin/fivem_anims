66cd462f9e32138fcb94267f6cfdec2255d5f24e
Martin <74150428+ProsteMartin@users.noreply.github.com>
2022-11-06 00:08:48 +0100

    Update cl_handsup.lua

--- anims/cl_handsup.lua
+++ anims/cl_handsup.lua
@@ -2,18 +2,20 @@
 local handsup = false
 
 RegisterCommand('handsup', function()
-    if not handsup then
-        local dict = "random@mugging3"
-        RequestAnimDict(dict)
-        while not HasAnimDictLoaded(dict) do
-            Wait(500)
+    if not IsPedInAnyVehicle(PlayerPedId(), false) then
+        if not handsup then
+            local dict = "random@mugging3"
+            RequestAnimDict(dict)
+            while not HasAnimDictLoaded(dict) do
+                Wait(500)
+            end
+            TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 2.5, 2.5, -1, 50, 0, false, false, false)
+            RemoveAnimDict(dict)
+            handsup = true
+        else
+            handsup = false
+            ClearPedTasks(PlayerPedId())
         end
-        TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 2.5, 2.5, -1, 50, 0, false, false, false)
-        RemoveAnimDict(dict)
-        handsup = true
-    else
-        handsup = false
-        ClearPedTasks(PlayerPedId())
     end
 end)
 
