dc6bc4356320a93f61f8fc6122fc4011679b37c8
Martin <74150428+ProsteMartin@users.noreply.github.com>
2022-11-06 00:07:05 +0100

    Update cl_crossarms.lua

--- anims/cl_crossarms.lua
+++ anims/cl_crossarms.lua
@@ -2,18 +2,20 @@
 local crossarms = false
 
 RegisterCommand('crossarms', function()
-    if not crossarms then
-        local dict = "anim@amb@nightclub@peds@"
-        RequestAnimDict(dict)
-        while not HasAnimDictLoaded(dict) do
-            Wait(500)
+    if not IsPedInAnyVehicle(PlayerPedId(), false) then
+        if not crossarms then
+                local dict = "anim@amb@nightclub@peds@"
+                RequestAnimDict(dict)
+                while not HasAnimDictLoaded(dict) do
+                    Wait(500)
+                end
+                TaskPlayAnim(PlayerPedId(), dict, "rcmme_amanda1_stand_loop_cop", 2.5, 2.5, -1, 50, 0, false, false, false)
+                RemoveAnimDict(dict)
+                crossarms = true
+        else
+            crossarms = false
+            ClearPedTasks(PlayerPedId())
         end
-        TaskPlayAnim(PlayerPedId(), dict, "rcmme_amanda1_stand_loop_cop", 2.5, 2.5, -1, 50, 0, false, false, false)
-        RemoveAnimDict(dict)
-        crossarms = true
-    else
-        crossarms = false
-        ClearPedTasks(PlayerPedId())
     end
 end)
 
