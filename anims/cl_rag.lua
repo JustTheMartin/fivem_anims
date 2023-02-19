f65a9f5514874c1f6901d9108be01ca40d0b5cee
Martin <74150428+ProsteMartin@users.noreply.github.com>
2023-02-19 16:45:37 +0100

    Update cl_rag.lua

--- anims/cl_rag.lua
+++ anims/cl_rag.lua
@@ -2,12 +2,12 @@
 RegisterCommand("rag", function(source, args, raw)
 	if not ragdol then
 		ragdol = true
-        Citizen.CreateThread(function()
-            while ragdol do
-                SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
-                Citizen.Wait(0)
-            end
-        end)
+            Citizen.CreateThread(function()
+                    while ragdol do
+                        SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
+                        Citizen.Wait(0)
+                    end
+            end)
 	else
 		ragdol = false
 	end
