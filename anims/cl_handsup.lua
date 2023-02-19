0d8bf609ad5a689e4a07e9ef1ec04b15eea86c57
Martin <74150428+ProsteMartin@users.noreply.github.com>
2023-02-19 16:37:46 +0100

    Fix animation that sometimes did not start

--- anims/cl_handsup.lua
+++ anims/cl_handsup.lua
@@ -1,18 +1,16 @@
 -- HANDSUP
-local handsup = false
+local ped = PlayerPedId()
 
 RegisterCommand('handsup', function()
-    if not IsPedInAnyVehicle(PlayerPedId(), false) then
+    if not IsPedInAnyVehicle(ped, false) and not IsPedFalling(ped) then
         local dict = "random@mugging3"
-        if not handsup then
+        if not IsEntityPlayingAnim(oed, dict, "handsup_standing_base", 3) then
             RequestAnimDict(dict)
             while not HasAnimDictLoaded(dict) do
                 Wait(500)
             end
             TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 2.5, 2.5, -1, 50, 0, false, false, false)
-            handsup = true
         else
-            handsup = false
             StopAnimTask(PlayerPedId(), dict, "handsup_standing_base", -2.5)
             RemoveAnimDict(dict)
         end
