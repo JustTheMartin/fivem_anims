f659a52300fa3be7d4dc763cd270067a7737d230
Martin <74150428+ProsteMartin@users.noreply.github.com>
2023-04-10 18:05:10 +0200

    oed is a good pronunciation of ped

--- anims/cl_crossarms.lua
+++ anims/cl_crossarms.lua
@@ -4,7 +4,7 @@ RegisterCommand('crossarms', function()
     local ped = PlayerPedId()
     if not IsPedInAnyVehicle(ped, false) and not IsPedFalling(ped) then
         local dict = "anim@amb@nightclub@peds@"
-        if not IsEntityPlayingAnim(oed, dict, "rcmme_amanda1_stand_loop_cop", 3) then
+        if not IsEntityPlayingAnim(ped, dict, "rcmme_amanda1_stand_loop_cop", 3) then
             RequestAnimDict(dict)
             while not HasAnimDictLoaded(dict) do
                 Wait(500)
