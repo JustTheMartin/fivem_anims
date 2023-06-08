0c9be3f6f9849a10646cc1e0994f5ed7dc73871e
Martin <74150428+ProsteMartin@users.noreply.github.com>
2023-06-08 16:58:35 +0200

    Ped cant be out of nowhere

--- anims/cl_rag.lua
+++ anims/cl_rag.lua
@@ -1,17 +1,17 @@
 -- RAG 
 RegisterCommand("rag", function(source, args, raw)
-	local ped = PlayerPedId()
-	if not ragdol then
-		ragdol = true
-            Citizen.CreateThread(function()
-                    while ragdol do
-                        SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
-                        Citizen.Wait(0)
-                    end
-            end)
-	else
-		ragdol = false
-	end
+    if not ragdol then
+        ragdol = true
+        CreateThread(function()
+            local ped = PlayerPedId()
+            while ragdol do
+                SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
+                Wait(0)
+            end
+        end)
+    else
+        ragdol = false
+    end
 end, false)
 
 RegisterKeyMapping('rag', Config.RagName, 'keyboard', Config.RagBind)
