b7eca8c153681a756335310e0f730f14daf4b149
Martin <74150428+JustTheMartin@users.noreply.github.com>
2023-08-07 21:37:15 +0200

    I like this better

--- anims/cl_crossarms.lua
+++ anims/cl_crossarms.lua
@@ -1,17 +1,17 @@
 -- CROSSARMS
-
+local dict = "anim@amb@nightclub@peds@"
+local anim = "rcmme_amanda1_stand_loop_cop"
 RegisterCommand('crossarms', function()
     local ped = PlayerPedId()
     if not IsPedInAnyVehicle(ped, false) and not IsPedFalling(ped) then
-        local dict = "anim@amb@nightclub@peds@"
-        if not IsEntityPlayingAnim(ped, dict, "rcmme_amanda1_stand_loop_cop", 3) then
+        if not IsEntityPlayingAnim(ped, dict, anim, 3) then
             RequestAnimDict(dict)
             while not HasAnimDictLoaded(dict) do
                 Wait(500)
             end
-            TaskPlayAnim(ped, dict, "rcmme_amanda1_stand_loop_cop", 2.5, 2.5, -1, 50, 0, false, false, false)
+            TaskPlayAnim(ped, dict, anim, 2.5, 2.5, -1, 50, 0, false, false, false)
         else
-            StopAnimTask(ped, dict, "rcmme_amanda1_stand_loop_cop", -2.5)
+            StopAnimTask(ped, dict, anim, -2.5)
             RemoveAnimDict(dict)
         end
     end
